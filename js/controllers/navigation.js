11eaffb3d7818500d8d0bd07f37745648e4494bf
StelianDimitrov <Brglz@users.noreply.github.com>
2020-08-23 14:04:28 +0300

    changed menu item style

--- js/controllers/navigation.js
+++ js/controllers/navigation.js
@@ -1,20 +1,19 @@
 window.addEventListener('load', function () {
     const menuButton = document.querySelector('.navbar__mobile i')
     const menu = document.querySelector('.navbar__mobile-menu')
-    menuButton.addEventListener('click', function () {
-        if (menu.style.visibility == 'hidden') {
 
-            menu.style.visibility = 'visible';
-            menu.style.height = 'max-content';
-            menu.style.opacity = '1';
+    menuButton.addEventListener('click', () => {
+
+        if (menu.style.display == 'none') {
+
+            menu.style.display = 'flex';
 
         } else {
 
-            menu.style.visibility = 'hidden';
-            menu.style.height = '0';
-            menu.style.opacity = '0';
+            menu.style.display = 'none';
 
         }
-        console.log('asd');
+
+
     })
 })
