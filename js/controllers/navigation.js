7c7272ca00d81b24895ee9c457522cf94a90f66e
StelianDimitrov <Brglz@users.noreply.github.com>
2020-08-23 13:18:25 +0300

    added a way to show menu in responsive mode

---
+++ js/controllers/navigation.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function () {
+    const menuButton = document.querySelector('.navbar__mobile i')
+    const menu = document.querySelector('.navbar__mobile-menu')
+    menuButton.addEventListener('click', function () {
+        if (menu.style.visibility == 'hidden') {
+            menu.style.visibility = 'visible';
+            menu.style.height = 'max-content';
+            menu.style.opacity = '1';
+        } else {
+            menu.style.visibility = 'hidden';
+            menu.style.height = '0';
+            menu.style.opacity = '0';
+        }
+        console.log('asd');
+    })
+})
