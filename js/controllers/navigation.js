629aa57578fa63a8da5027736a69a038ef9c4aaf
StelianDimitrov <Brglz@users.noreply.github.com>
2020-08-23 13:35:25 +0300

    added clickable menu items in responsive mode

--- js/controllers/navigation.js
+++ js/controllers/navigation.js
@@ -3,13 +3,17 @@ window.addEventListener('load', function () {
     const menu = document.querySelector('.navbar__mobile-menu')
     menuButton.addEventListener('click', function () {
         if (menu.style.visibility == 'hidden') {
+
             menu.style.visibility = 'visible';
             menu.style.height = 'max-content';
             menu.style.opacity = '1';
+
         } else {
+
             menu.style.visibility = 'hidden';
             menu.style.height = '0';
             menu.style.opacity = '0';
+
         }
         console.log('asd');
     })
