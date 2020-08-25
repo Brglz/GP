9b94a946deeb9ad3628fba9a2d1a86c738daf2e6
StelianDimitrov <Brglz@users.noreply.github.com>
2020-08-25 19:29:05 +0300

    edit homepage videos

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,4 +1,31 @@
 window.addEventListener('load', function () {
+    const viewportFilm = document.querySelector('.home-main-content-box-films');
+    let film = document.getElementById('myFilm')
+    viewportFilm.addEventListener('mouseover', function (e) {
+        const isPlaying = film.currentTime > 0 && !film.paused && !film.ended
+            && film.readyState > 2;
+
+        if (!isPlaying) {
+            film.play();
+        }
+        console.log(e.target);
+
+    }, false);
+
+    viewportFilm.addEventListener('mouseout', function (e) {
+        const isPlaying = film.currentTime > 0 && !film.paused && !film.ended
+            && film.readyState > 2;
+
+        if (isPlaying) {
+            film.pause();
+            // video.currentTime = 0;
+        }
+
+    }, false);
+
+
+
+
     const viewport = document.querySelector('.home-main-content-box-videos');
     let video = document.getElementById('myVideo')
     viewport.addEventListener('mouseover', function (e) {
@@ -8,16 +35,17 @@ window.addEventListener('load', function () {
         if (!isPlaying) {
             video.play();
         }
-        video.play
+        console.log(e.target);
+
     }, false);
 
-    viewport.addEventListener('mouseout', async function (e) {
+    viewport.addEventListener('mouseout', function (e) {
         const isPlaying = video.currentTime > 0 && !video.paused && !video.ended
             && video.readyState > 2;
 
         if (isPlaying) {
-            await video.pause();
-            video.currentTime = 0;
+            video.pause();
+            // video.currentTime = 0;
         }
 
     }, false);
